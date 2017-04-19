b230e2bc9ce5e06e919f96a9f7fb2766b5d3d569
Ajay Gupta <ajay.gupta@innovaccer.com>
2017-04-19 06:31:01 +0530

    Added seconds Handson with Graph

---
+++ handson/tensorflow_handson_second.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+# Number Constant
+# Name is added to get name on graph, otherwise TensorFlow will give name as const and const_1
+const_one = tf.constant(1.0, tf.float32, name="const_one")
+const_two = tf.constant(2.0, name="const_two")
+
+# Addition of Constant
+# Name is added to get name on graph
+addition = tf.add(const_one, const_two, name="add")
+
+with tf.Session() as sess:
+    # Write session graph to file
+    writer = tf.summary.FileWriter('./graphs', sess.graph)
+    print(sess.run(addition))
+
+# Session Graph can be viewed on TensorBoard using this command
+# tensorboard --logdir="./graphs"
+writer.close()
